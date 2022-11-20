ad8170aade2a95ebef0e9e0dd384d98aff22e3e6
Eskil Aursand <eskil.aursand@gmail.com>
2022-11-20 23:06:19 +0100

    Chapter 9 (metals) done

--- src/lib/Material.h
+++ src/lib/Material.h
@@ -42,4 +42,43 @@ namespace Raytracing
         Color3 m_albedo;
     };
 
+    class Metal : public Material
+    {
+    public:
+        Metal()
+            : Metal(Color3(0.8, 0.8F, 0.8F), 0.05F)
+        {}
+
+        Metal(const Color3 &albedo)
+            : Metal(albedo, 0.05F)
+        {}
+
+        Metal(const Color3 &albedo, float roughness)
+            : m_albedo(albedo)
+            , m_roughness(roughness)
+        {
+            if(m_roughness < 0 || m_roughness >= 1)
+            {
+                throw std::runtime_error("Roughness must be in [0,1)");
+            }
+        }
+
+        virtual std::optional<ScatterResult>
+        scatter(const Ray &incomingRay, const Point3 &hitPoint, const Vec3 &hitNormal) const override
+        {
+            const auto v = unit_vector(incomingRay.direction());
+            const auto reflectedDirection = v - 2 * dot(v, hitNormal) * hitNormal;
+            const auto scatteredRay = Ray(hitPoint, reflectedDirection + m_roughness * random_point_in_unit_sphere());
+            if(dot(scatteredRay.direction(), hitNormal) <= 0.0F)
+            {
+                return std::nullopt;
+            }
+            return ScatterResult{ m_albedo, scatteredRay };
+        }
+
+    private:
+        Color3 m_albedo;
+        float m_roughness;
+    };
+
 } // namespace Raytracing
